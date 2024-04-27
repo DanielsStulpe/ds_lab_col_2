2627f6e606f44981f67a19fe80023b5f49ea51fb
DanielsStulpe <144322470+DanielsStulpe@users.noreply.github.com>
2024-04-27 11:24:52 +0000

    25.04.2024

--- heap.py
+++ heap.py
@@ -69,21 +69,29 @@ def find_kth_smallest(nums, k):
     ###################################
 
 
+nums = input("Ievadiet skaiļu rindu : ").split(" ")
+try:
+    k = int(input("Ievadiet skaitļi k : "))
+except:
+    k = 0
+if k > 0 and k <= len(nums):
+    print("Output: ", find_kth_smallest(nums, k))
+else :
+    print("Error")
 
+# # Test cases
+# nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
+# ks = [2, 3, 4, 7]
+# expected_outputs = [2, 3, 4, 5]
 
-# Test cases
-nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
-ks = [2, 3, 4, 7]
-expected_outputs = [2, 3, 4, 5]
-
-for i in range(len(nums)):
-    print(f'Test case {i+1}...')
-    print(f'Input: {nums[i]} with k = {ks[i]}')
-    result = find_kth_smallest(nums[i], ks[i])
-    print(f'Output: {result}')
-    print(f'Expected output: {expected_outputs[i]}')
-    print(f'Test passed: {result == expected_outputs[i]}')
-    print('---------------------------------------')
+# for i in range(len(nums)):
+#     print(f'Test case {i+1}...')
+#     print(f'Input: {nums[i]} with k = {ks[i]}')
+#     result = find_kth_smallest(nums[i], ks[i])
+#     print(f'Output: {result}')
+#     print(f'Expected output: {expected_outputs[i]}')
+#     print(f'Test passed: {result == expected_outputs[i]}')
+#     print('---------------------------------------')
 
 
 """
